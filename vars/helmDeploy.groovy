d0088fa481a314baaca76a76f25f57526a0356dd
Apurv <gujjarapurv1801@gmail.com>
2026-03-14 10:24:17 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -1,7 +1,5 @@
 def call(config) {
 
-    sh "kubectl get namespace ${config.namespace}"
-
     sh """
     helm upgrade --install ${config.releaseName} ${config.chartPath} \
     --namespace ${config.namespace} \
