8cd12fe73c0cfc15915932f2783b2f94b77235cd
Apurv <gujjarapurv1801@gmail.com>
2026-03-14 10:54:27 +0530

    Update helmDeploy.groovy

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -3,6 +3,7 @@ def call(config) {
     sh """
     helm upgrade --install ${config.releaseName} ${config.chartPath} \
     --namespace ${config.namespace} \
+    --create-namespace \
     -f ${config.valuesFile} \
     --wait \
     --timeout 5m
