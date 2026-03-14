aa1b684b8fee572c6a9eb028f2db9ef2de83f7bd
Apurv <gujjarapurv1801@gmail.com>
2026-03-14 12:07:43 +0530

    Delete vars/sonarScan.groovy

--- vars/sonarScan.groovy
+++
@@ -1,9 +0,0 @@
-def call(sonarHome) {
-    withSonarQubeEnv('sonar') {
-        sh """
-        ${sonarHome}/bin/sonar-scanner \
-        -Dsonar.projectName=simple-notes-app \
-        -Dsonar.projectKey=simple-notes-app
-        """
-    }
-}
