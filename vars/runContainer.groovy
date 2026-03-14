f40ded04ef12547af2f16cbcc6fa989c13af73ab
Apurv <gujjarapurv1801@gmail.com>
2026-03-14 12:08:52 +0530

    Delete vars/runContainer.groovy

--- vars/runContainer.groovy
+++
@@ -1,9 +0,0 @@
-def call(imageName) {
-    sh '''
-    if [ "$(docker ps -q -f name=notes-app)" ]; then
-        echo "Container already running"
-    else
-        docker run -d -p 5000:5000 --name notes-app '"${imageName}:latest"'
-    fi
-    '''
-}
