051e5a63bf80c404552dc0dfa68895120fb1b446
Apurv <gujjarapurv1801@gmail.com>
2026-03-16 12:09:10 +0530

    Delete vars/authenticateGCP.groovy

--- vars/authenticateGCP.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    withCredentials([file(credentialsId: 'gcp-artifact-key', variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
-        sh '''
-        gcloud auth activate-service-account --key-file=$GOOGLE_APPLICATION_CREDENTIALS
-        gcloud auth configure-docker us-central1-docker.pkg.dev --quiet
-        '''
-    }
-}
